7b8fe57a22259700c9724c9e0e86697f81e13f75
Lika <lika.shugurova@gmail.com>
2018-11-18 18:22:46 +0200

    Update DbManager.java

--- src/main/java/DbManager.java
+++ src/main/java/DbManager.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class DbManager {
     static final String DB_URL = "jdbc:postgresql://localhost:5432/statistics_db";
     static final String USER = "postgres";
-    static final String PASS = "Armagedon12";
+    static final String PASS = "postgres";
 
     public void AddFile(FileInfoDto fileInfo) {
         Connection conn = null;
